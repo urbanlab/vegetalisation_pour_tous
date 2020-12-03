c882daed24ab1bd720f2b096825fdd5c3b69895b
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-12-03 15:21:25 +0100

    Page mon Espace

---
+++ js/include.js
@@ -0,0 +1,36 @@
+// Fonction d'include pour pas s'emmerder trop dans l'intégration
+window.onload = function() {
+	var elements = document.getElementsByTagName('*'),
+		i;
+	for (i in elements) {
+		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
+			fragment(elements[i], elements[i].getAttribute('data-include'));
+		}
+	}
+	function fragment(el, url) {
+		var localTest = /^(?:file):/,
+			xmlhttp = new XMLHttpRequest(),
+			status = 0;
+
+		xmlhttp.onreadystatechange = function() {
+			/* if we are on a local protocol, and we have response text, we'll assume
+ *  				things were sucessful */
+			if (xmlhttp.readyState == 4) {
+				status = xmlhttp.status;
+			}
+			if (localTest.test(location.href) && xmlhttp.responseText) {
+				status = 200;
+			}
+			if (xmlhttp.readyState == 4 && status == 200) {
+				el.outerHTML = xmlhttp.responseText;
+			}
+		}
+
+		try { 
+			xmlhttp.open("GET", url, true);
+			xmlhttp.send();
+		} catch(err) {
+			/* todo catch error */
+		}
+	}
+}
