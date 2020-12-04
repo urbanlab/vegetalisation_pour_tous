089c68c522f809e2b992648364a9b0ae94698060
quentinScript <quentin.bozon1@gmail.com>
2020-12-04 09:40:44 +0100

    header

--- js/arboricoop.js
+++ js/arboricoop.js
@@ -6,7 +6,7 @@ const arbustes = {
         "dimension":"",
         "forme":"touffe",
         "descriptif":"",
-        "emplacementImage":"2.png"
+        "emplacementImage":"2.jpg"
     },
     "BERBERIS agregata":{
         "origine":"",
@@ -14,7 +14,7 @@ const arbustes = {
         "dimension": "H. 1,50m",
         "forme":"touffe",
         "descriptif":"Très rustique, fleurs jaunes, fruits rose rouge très nombreux et très décoratifs, feuillage fin rougissant à l'automne. Excellent pour haie dense et massif arbustif. Densité : 1/m².",
-        "emplacementImage":"3.png"
+        "emplacementImage":"3.jpg"
     },
     "MAHONIA japonica 'Beali'":{
         "origine":"Chine",
@@ -22,7 +22,7 @@ const arbustes = {
         "dimension": "H. 1.50m/2m",
         "forme":"touffe",
         "descriptif":"Port dressé. Très rustique, tous types de sols, ce mahonia a l'avantage de mieux vieillir qu'aquifolium. Très apprécié pour son feuillage vert pouvant être veiné de jaune et de rouge, selon les terrains et l'exposition. Fleurs jaune soufre en longues grappes étalées sur le feuillage au sommet des rameaux. Très employé en massif, jardinière, rocaille, banquette.",
-        "emplacementImage":"2.png"
+        "emplacementImage":"2.jpg"
     },
     "BERBERIS thunbergii vert":{
         "origine":"",
@@ -30,7 +30,7 @@ const arbustes = {
         "dimension": "",
         "forme":"",
         "descriptif":"Espèce très intéressante, pour haie basse ou moyenne, compacte, très belle coloration automnale rouge orangé, fruits rouges décoratifs après la chute des feuilles. Tous types de sols. Densité de plantation au ml : écartement égal à la taille plantée (ex. sujet de 40/60 = écartement de 40 cm).",
-        "emplacementImage":"1.png"
+        "emplacementImage":"1.jpg"
     }
 };
 
@@ -82,7 +82,7 @@ function peuplerContenuQuoiPlanter(nombreDeBlocACreer=16, idBlocAremplir = 'list
         let nouveauBlocPlante = document.createElement("img");
         nouveauBlocPlante.classList.add('blocPlanteAChoisir');
         const numeroArbuste = i%Object.keys(arbustes).length;
-        nouveauBlocPlante.src='img/arbre/' + arbustes[Object.keys(arbustes)[numeroArbuste]].emplacementImage;
+        nouveauBlocPlante.src='img/arbre/' + 'arbre'+ arbustes[Object.keys(arbustes)[numeroArbuste]].emplacementImage;
         nouveauBlocPlante.onclick = function() { afficherDetailsPlante(this); };
         contenant.appendChild(nouveauBlocPlante);
         i++;
